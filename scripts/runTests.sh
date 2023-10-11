1263795b5a2fb91b3c3f84bbd0c5ab0608d4e89b
sorgom <110917257+sorgom@users.noreply.github.com>
2023-10-11 12:31:54 +0200

    issue 5 resolved (#7)

---
+++ scripts/runTests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+mydir=$(dirname $0)
+$mydir/../testing/bin/moduletests
---
+++ testing/tests/basetests/TestPack.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef TESTPACK_H
+#define TESTPACK_H
+
+#include <baselib/BaseTypes.h>
+
+namespace test
+{
+
+    struct Unpacked
+    {
+        UINT8 memb1;
+        UINT32 memb2;
+    };
+
+    #include <baselib/packBegin.h>
+
+    struct Packed
+    {
+        UINT8 memb1;
+        UINT32 memb2;
+    };
+
+    #include <baselib/packEnd.h>
+
+} 
+#endif // _H
