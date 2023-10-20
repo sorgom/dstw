79c9e7912573ecadddfc050d700ff35d2acede38
sorgom <110917257+sorgom@users.noreply.github.com>
2023-10-21 00:46:08 +0200

    diagram changes

--- application/ifs/I_Dispatcher.h
+++ application/ifs/I_Dispatcher.h
@@ -22,19 +22,26 @@ public:
     //  - element name
     //  - subsystem id
     //  - own processing element position
-    //  and recieve:
+    //  and shall recieve:
     //  - dispatcher id
     //  ============================================================
     virtual INT32 assign(const ElementName& name, E_Subsys subs, UINT32 pos) = 0;
 
     //  ============================================================
     //  when sending telegrams
-    //  subsytems retieve the assigned name with:
+    //  subsytems shall retieve the assigned name with:
     //  - dispatcher id returned at assignement
     //  ============================================================
     virtual bool label(ElementName& name, UINT32 id) const = 0;
 
+    //  ============================================================
+    //  field states shall be dispatched to subsystems
+    //  ============================================================
     virtual bool dispatch(const FldState& tele) const = 0;
+
+    //  ============================================================
+    //  GUI commands shall be dispatched to subsystems
+    //  ============================================================
     virtual bool dispatch(const GuiCmd&   tele) const = 0;
 
 };
---
+++ application/ifs/I_Loader.h
@@ -0,0 +1,20 @@
+//  ============================================================
+//  interface Loader
+//  the Loader is called at sytem start and shall:
+//  -   reset I_Dispatcher
+//  -   distribute load to subsytems
+//  -   call indexing of I_Dispatcher
+//  -   in case of any subsystem load failure
+//      -   reset I_Dispatcher
+//  ============================================================
+//  created by Manfred Sorgo
+#pragma once
+#ifndef I_LOADER_H
+#define I_LOADER_H
+
+class I_Loader
+{
+
+};
+
+#endif // H_
