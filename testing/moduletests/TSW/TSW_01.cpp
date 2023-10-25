16d333c7261cbcab23d644e8c84f757a64001baf
sorgom <110917257+sorgom@users.noreply.github.com>
2023-10-25 10:37:43 +0200

    tests TSW

--- testing/moduletests/TSW/TSW_01.cpp
+++ testing/moduletests/TSW/TSW_01.cpp
@@ -2,28 +2,14 @@
 //  test of module TSW
 //  ============================================================
 //  created by Manfred Sorgo
+
 #include <testlib/TestGroupBase.h>
 #include <TSW/TSW.h>
 
 namespace test
 {
-    class TestGroupTSW : public TestGroupBase
-    {
-    protected:
-        const UINT32 mId;
-        TSW mSUT;
-        M_TSW_Hub& mHub;
 
-        inline TestGroupTSW():
-            mId(12345),
-            mSUT(mId),
-            mHub(m_TSW_Hub())
-        {
-            mock_TSW_Hub();
-        }
-    };
-
-    TEST_GROUP_BASE(TSW_01, TestGroupTSW)
+    TEST_GROUP_BASE(TSW_01, TestGroupBase)
     {};
 
     //  test type: equivalence class test
@@ -31,70 +17,101 @@ namespace test
     TEST(TSW_01, T01)
     {
         SETUP()
+        mockAll();
+        const M_TSW_Hub& hub = m_TSW_Hub();
+        const UINT32 id = 123;
+        TSW SUT(id);
+
         STEP(1)
-        //  stimulation: WU() to initial state
-        //  reaction   : none
-        mSUT.fromGui(TSW_GUI_GMD_WU);
+        hub.expectToGui(id, TSW_TO_GUI_DEFECT);
+        SUT.fromFld(TSW_FROM_FLD_DEFECT);
         CHECK_N_CLEAR()
 
         STEP(2)
-        //  stimulation: feedback from field: LEFT
-        //  reaction   : cmd to GUI: LEFT
-        mHub.expectToGui(mId, TSW_TO_GUI_LEFT);
-        mSUT.fromFld(TSW_FROM_FLD_LEFT);
+        SUT.fromFld(TSW_FROM_FLD_DEFECT);
         CHECK_N_CLEAR()
 
         STEP(3)
-        //  stimulation: WU()
-        //  reaction   :
-        //      cmd to field: RIGHT
-        //      cmd to GUI: WAIT_RIGHT
-        mHub.expectToFld(mId, TSW_TO_FLD_RIGHT);
-        mHub.expectToGui(mId, TSW_TO_GUI_WAIT_RIGHT);
-        mSUT.fromGui(TSW_GUI_GMD_WU);
+        SUT.fromGui(TSW_GUI_GMD_WU);
         CHECK_N_CLEAR()
 
         STEP(4)
-        //  stimulation: WU() repeated
-        //  reaction   : none
-        mSUT.fromGui(TSW_GUI_GMD_WU);
+        hub.expectToGui(id, TSW_TO_GUI_UNDEF);
+        SUT.fromFld(TSW_FROM_FLD_UNDEF);
         CHECK_N_CLEAR()
 
         STEP(5)
-        //  stimulation: feedback from field: RIGHT
-        //  reaction   : cmd to GUI: RIGHT
-        mHub.expectToGui(mId, TSW_TO_GUI_RIGHT);
-        mSUT.fromFld(TSW_FROM_FLD_RIGHT);
+        SUT.fromFld(TSW_FROM_FLD_UNDEF);
         CHECK_N_CLEAR()
 
         STEP(6)
-        //  stimulation: WU()
-        //  reaction   :
-        //      cmd to field: LEFT
-        //      cmd to GUI: WAIT_LEFT
-        mHub.expectToFld(mId, TSW_TO_FLD_LEFT);
-        mHub.expectToGui(mId, TSW_TO_GUI_WAIT_LEFT);
-        mSUT.fromGui(TSW_GUI_GMD_WU);
+        SUT.fromGui(TSW_GUI_GMD_WU);
         CHECK_N_CLEAR()
 
         STEP(7)
-        //  stimulation: WU() repeated
-        //  reaction   : none
-        mSUT.fromGui(TSW_GUI_GMD_WU);
+        hub.expectToGui(id, TSW_TO_GUI_LEFT);
+        SUT.fromFld(TSW_FROM_FLD_LEFT);
         CHECK_N_CLEAR()
 
         STEP(8)
-        //  stimulation: feedback from field: LEFT
-        //  reaction   : cmd to GUI: LEFT
-        mHub.expectToGui(mId, TSW_TO_GUI_LEFT);
-        mSUT.fromFld(TSW_FROM_FLD_LEFT);
+        SUT.fromFld(TSW_FROM_FLD_LEFT);
         CHECK_N_CLEAR()
 
         STEP(9)
-        //  stimulation: feedback from field: LEFT repeated
-        //  reaction   : none
-        mSUT.fromFld(TSW_FROM_FLD_LEFT);
+        hub.expectToFld(id, TSW_TO_FLD_RIGHT);
+        hub.expectToGui(id, TSW_TO_GUI_WAIT_RIGHT);
+        SUT.fromGui(TSW_GUI_GMD_WU);
         CHECK_N_CLEAR()
-    }
 
+        STEP(10)
+        SUT.fromGui(TSW_GUI_GMD_WU);
+        CHECK_N_CLEAR()
+
+        STEP(11)
+        hub.expectToGui(id, TSW_TO_GUI_RIGHT);
+        SUT.fromFld(TSW_FROM_FLD_RIGHT);
+        CHECK_N_CLEAR()
+
+        STEP(12)
+        SUT.fromFld(TSW_FROM_FLD_RIGHT);
+        CHECK_N_CLEAR()
+
+        STEP(13)
+        hub.expectToFld(id, TSW_TO_FLD_LEFT);
+        hub.expectToGui(id, TSW_TO_GUI_WAIT_LEFT);
+        SUT.fromGui(TSW_GUI_GMD_WU);
+        CHECK_N_CLEAR()
+
+        STEP(14)
+        SUT.fromGui(TSW_GUI_GMD_WU);
+        CHECK_N_CLEAR()
+
+        STEP(15)
+        hub.expectToFld(id, TSW_TO_FLD_RIGHT);
+        hub.expectToGui(id, TSW_TO_GUI_WAIT_RIGHT);
+        SUT.fromGui(TSW_GUI_GMD_RIGHT);
+        CHECK_N_CLEAR()
+
+        STEP(16)
+        SUT.fromGui(TSW_GUI_GMD_RIGHT);
+        CHECK_N_CLEAR()
+
+        STEP(17)
+        hub.expectToFld(id, TSW_TO_FLD_LEFT);
+        hub.expectToGui(id, TSW_TO_GUI_WAIT_LEFT);
+        SUT.fromGui(TSW_GUI_GMD_LEFT);
+        CHECK_N_CLEAR()
+
+        STEP(18)
+        SUT.fromGui(TSW_GUI_GMD_LEFT);
+        CHECK_N_CLEAR()
+
+        STEP(19)
+        SUT.fromFld(-1);
+        CHECK_N_CLEAR()
+
+        STEP(20)
+        SUT.fromGui(-1);
+        CHECK_N_CLEAR()
+   }
 }
