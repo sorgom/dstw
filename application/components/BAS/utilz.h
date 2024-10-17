b201b73df5d996508a2364f7630b012fdbedd6ca
sorgom <110917257+sorgom@users.noreply.github.com>
2024-10-17 14:54:05 +0200

    getn template

--- application/components/BAS/utilz.h
+++ application/components/BAS/utilz.h
@@ -11,12 +11,19 @@
 template <size_t N>
 using data_tn = UINT8[N];
 
-//  get subset of ComData params
-template <size_t N>
-const data_tn<N>& getn(const ComData& data)
+//  get byte subset of an object
+template <class T, size_t N, size_t P=0>
+inline const data_tn<N>& getn(const T& obj)
 {
-    static_assert(N <= ComDataSize);
-    return *reinterpret_cast<const data_tn<N>*>(&data);
+    static_assert(P + N <= sizeof(T));
+    return *(reinterpret_cast<const data_tn<N>*>(&obj) + P);
+}
+
+//  get byte subset of ComData
+template <size_t N, size_t P=0>
+inline const data_tn<N>& getn(const ComData& obj)
+{
+    return getn<ComData, N, P>(obj);
 }
 
 #endif // _H
